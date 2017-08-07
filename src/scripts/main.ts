52e67e2d5a05a2c3b443c5ad3c219d575637aefb
Kirill Vorobiov <mishkacod@gmail.com>
2017-08-07 17:29:14 +0300

    add md-side-nav

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,7 +4,8 @@ require('./polyfills');
 // Custom styles.
 require('../styles/main.sass');
 
-import { MainModule }              from './app/main.module';
+import { MainModule } from './app/main.module';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 platformBrowserDynamic().bootstrapModule(MainModule);
