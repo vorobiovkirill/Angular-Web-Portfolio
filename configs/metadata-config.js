f54cf39faf8b5c9e1275b3f563454b0cca5fcb16
Kirill Vorobiov <mishkacod@gmail.com>
2017-08-11 15:49:19 +0300

    test 2

--- configs/metadata-config.js
+++ configs/metadata-config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	title: 'Web Portfolio',
 	description: 'Web Portfolio by Vorobiov Kirill',
-	baseUrl: 'https://vorobyovkirill/Angular-Web-Portfolio'
+	baseUrl: 'https://vorobyovkirill.github.io/Angular-Web-Portfolio/'
 };
