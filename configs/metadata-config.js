0c03e021e78270f06774d87c018286fc727b0fbd
Kirill Vorobiov <mishkacod@gmail.com>
2017-08-11 15:55:30 +0300

    update

--- configs/metadata-config.js
+++ configs/metadata-config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	title: 'Web Portfolio',
 	description: 'Web Portfolio by Vorobiov Kirill',
-	baseUrl: 'https://vorobyovkirill.github.io/Angular-Web-Portfolio/'
+	baseUrl: '/'
 };
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const config = {
 
 	output: {
 		publicPath: '/',
-		path: path.resolve(__dirname, 'docs'),
+		path: path.resolve(__dirname, 'dist'),
 		filename: 'js/[name].bundle.js'
 	},
 
@@ -45,7 +45,7 @@ const config = {
 	 * @link https://webpack.github.io/docs/webpack-dev-server.html
 	 */
 	devServer: {
-		contentBase: path.resolve(__dirname, 'docs'),
+		contentBase: path.resolve(__dirname, 'dist'),
 		compress: true,
 		historyApiFallback: true,
 		port: 8080,
